f65dad67e61245dc1313e3a387c990982082f7db
akashmondal2000 <mondalakash494@gmail.com>
2024-04-17 15:10:35 +0530

    cancle backdrpo by the clicking cancle button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ const addMovieModal = document.getElementById("add-modal");
 
 const startAddMovieButton = document.getElementById("add-movie-btn");
 const backdrop = document.getElementById("backdrop");
+const cancleAddMovieButton = document.querySelector(".btn--passive");
 
 // for use this backdrop another place also i will create a function
 const toggleBackdrop = ()=>{
@@ -23,5 +24,10 @@ const backdropClickHandler = ()=>{
     toggleMovieModal();
 }
 
+const cancleMovie = ()=>{
+    toggleMovieModal();
+}
+
 startAddMovieButton.addEventListener("click",toggleMovieModal);
 backdrop.addEventListener("click",backdropClickHandler);
+cancleAddMovieButton.addEventListener('click',cancleMovie);
